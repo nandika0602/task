1ce986ecf1fbfe3aeee28e2dd996cc435bc167a2
nandika0602 <nandika.d0602@gmail.com>
2024-01-15 23:38:08 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState(['a', 'b']);
+  const [filteredList, setFilteredList] = useState([])
+  useEffect(  () => {
+     fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {return res.json()})
+    .then((data) => {
+      setList(data)
+    })
+  },[])
+
+  useEffect(() => {
+    console.log(filteredList);
+  }, [filteredList])
+  const add = (e) => {
+    // console.log(list);
+    const l = list.filter(li => li.name.toLowerCase().includes(e.target.value.toLowerCase()))
+    console.log(l);
+    setFilteredList([...filteredList, ...l])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      {/* <h1 className="text-3xl">Hii</h1> */}
+      <div className="justify-center items-center m-auto border border-black p-3 mt-6">
+        {filteredList.map((l, i) => {
+          return (<div key={l.id}>
+            {console.log(l)}
+            <span>{l['name']}tag</span>
+            <span>{l.id}items</span>
+          </div>)
+        })}
+      <input type="text" placeholder="search the name" onKeyDown={e => (e.key === "Enter" ? add(e) : null)} />
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
